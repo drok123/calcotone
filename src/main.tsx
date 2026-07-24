8485592b2947c0f9fe2a69960316a4745fe2ba62
drok123 <52803485+drok123@users.noreply.github.com>
2026-07-24 09:47:15 -0500

    Load signal rail backplane layering

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import './components/effects/ModuleViewport.css'
+import './components/layout/SignalRailBackplane.css'
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
