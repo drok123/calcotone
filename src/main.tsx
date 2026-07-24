95da3c08283ac3467f89819f66d62bbbcb820440
drok123 <52803485+drok123@users.noreply.github.com>
2026-07-24 10:10:45 -0500

    Load faceplate resize fix after app styles

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import './index.css'
 import './components/effects/ModuleViewport.css'
 import './components/layout/SignalRailBackplane.css'
 import App from './App.tsx'
+import './components/layout/FaceplateResizeFix.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
