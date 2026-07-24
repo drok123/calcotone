389561fa87820f2d54d85b0ec5a0edee4780b8a7
drok123 <52803485+drok123@users.noreply.github.com>
2026-07-24 07:48:22 -0500

    Load compact module faceplate layout

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import './components/effects/ModuleViewport.css'
 import App from './App.tsx'
+import './components/effects/ModuleFaceplate.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
