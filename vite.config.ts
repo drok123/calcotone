c5894ace260b3c8b9a305b32bd3544ef495df5b2
drok123 <52803485+drok123@users.noreply.github.com>
2026-07-26 08:18:14 -0500

    Enable Dream Field composition refinement

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { serialRoutingTransform } from './build/serialRoutingTransform'
 import { signalLabUiTransform } from './build/signalLabUiTransform'
+import { dreamFieldCompositionTransform } from './build/dreamFieldCompositionTransform'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [serialRoutingTransform(), signalLabUiTransform(), react()],
+  plugins: [serialRoutingTransform(), signalLabUiTransform(), dreamFieldCompositionTransform(), react()],
 })
