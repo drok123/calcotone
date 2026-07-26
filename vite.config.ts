959976d0e11f46fb1e41980e672ff0ddd8f9a9e8
drok123 <52803485+drok123@users.noreply.github.com>
2026-07-25 23:57:36 -0500

    Mount Signal Lab UI transform

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { serialRoutingTransform } from './build/serialRoutingTransform'
+import { signalLabUiTransform } from './build/signalLabUiTransform'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [serialRoutingTransform(), react()],
+  plugins: [serialRoutingTransform(), signalLabUiTransform(), react()],
 })
