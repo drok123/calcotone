b7cc8a038ca51ab3cba91b39501a9d53cca018f2
drok123 <52803485+drok123@users.noreply.github.com>
2026-07-28 00:25:38 -0500

    Remove the retired Dream visual transform

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { serialRoutingTransform } from './build/serialRoutingTransform.js'
-import { dreamFieldCompositionTransform } from './build/dreamFieldCompositionTransform.js'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [serialRoutingTransform(), dreamFieldCompositionTransform(), react()],
+  plugins: [serialRoutingTransform(), react()],
 })
